4133e0ceddf43a47dab67915db18631d07e95692
fairofif <fairofif@gmail.com>
2022-03-26 00:50:50 +0700

    ganti move

--- src/main/java/com/monstersaku/util/Move.java
+++ src/main/java/com/monstersaku/util/Move.java
@@ -1,29 +1,75 @@
 package com.monstersaku.util;
 
 public class Move {
-    private int id;
     private String name;
     private ElementType elementType;
     private int accuracy;
     private int priority;
     private int ammunition;
-    private int basepower;
+    private int basePower;
 
-    public Move (){
 
+    private int kind;
+    /*
+        kind -> jenis move
+        1 = normal move
+        2 = special move
+        3 = default move
+        4 = status move    
+    */
+
+    public Move (String name, ElementType elementType, int accuracy, int priority, int ammunition, int basePower, int kind) {
+        this.name = name;
+        this.elementType = elementType;
+        this.accuracy = accuracy;
+        this.priority = priority;
+        this.ammunition = ammunition;
+        this.basePower = basePower;
+        this.kind = kind;
     }
 
-    public void DefaultMove (){
-        Move defaultmove = new Move();
-        defaultmove.id=99999;//idmove
-        defaultmove.ammunition=9999999;
-        defaultmove.basepower=50;
-        defaultmove.priority=0;
+    public void moveEffect (Monster attacker, Monster target) {
+        // normal move
+        if (this.kind == 1) {
+            if (this.ammunition != 0) {
+                double finaldamage= (double)Math.floor((this.basePower * (attacker.getStats().getAttack()/target.getStats().getDefense() + 2 ) * Math.random() ));
+                // ini belum effectivity + status kondisi
 
+                target.getStats().setHealtPoint(target.getStats().getHealthPoint() - finaldamage);
+
+                this.ammunition -= 1;
+            }
+            else {
+                System.out.println("Amunisi move ini habis");
+            }
+        }
+
+        // special move
+        else if (this.kind == 2) {
+            if (this.ammunition != 0) {
+                double finaldamage= (double)Math.floor((this.basePower * (attacker.getStats().getSpesialAttack()/target.getStats().getSpesialDefense() + 2 ) * Math.random() ));
+                // belum pake effectivity juga
+
+                target.getStats().setHealtPoint(target.getStats().getHealthPoint() - finaldamage);
+
+                this.ammunition -= 1;
+            }
+            else {
+                System.out.println("Amunisi move ini habis");
+            }
+        }
+
+        // default move
+        else if (this.kind == 3) {
+            // ga perlu ngecek amunisi karena unlimited
+            double finaldamage= (double)Math.floor((50 * (attacker.getStats().getAttack()/target.getStats().getDefense() + 2 ) * Math.random() ));
+            target.getStats().setHealtPoint(target.getStats().getHealthPoint() - finaldamage);
+        }
+        
+        // bonus move (status move)
+        else if (this.kind == 4) {
+            
+        }
     }
 
-    public void NormalMove (Monster player , Monster target){
-        float finaldamage= (float)Math.floor((this.basepower * (player.getStats().getAttack()/target.getStats().getDefense() + 2 ) * Math.random() ));
-        // ini belum effectivity + status kondisi
-    }
 }
